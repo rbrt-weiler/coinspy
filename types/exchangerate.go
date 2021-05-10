e62f966d289c7f1a67f089312c6a4e966226230b
Robert WEILER <rbrt-weiler@mailbox.org>
2021-05-10 21:52:43 +0200

    Fix wrong Sprintf() argument count.

--- types/exchangerate.go
+++ types/exchangerate.go
@@ -17,7 +17,7 @@ type ExchangeRate struct {
 }
 
 func (r *ExchangeRate) String() string {
-	return fmt.Sprintf("1 %s = %f %s (on %s as of %s)", r.Coin, r.Rate, r.Fiat, r.ProviderWithMarket, r.Market, r.AsOf.Format(time.RFC3339))
+	return fmt.Sprintf("1 %s = %f %s (on %s as of %s)", r.Coin, r.Rate, r.Fiat, r.ProviderWithMarket, r.AsOf.Format(time.RFC3339))
 }
 
 func (r *ExchangeRate) StringCompact() string {
