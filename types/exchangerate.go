19c765106f6b34d37421c3fd4b4fce2c89254aee
Robert WEILER <rbrt-weiler@mailbox.org>
2021-07-04 13:50:51 +0200

    Type changes to handle calculating actual portfolio values.

--- types/exchangerate.go
+++ types/exchangerate.go
@@ -17,6 +17,8 @@ type ExchangeRate struct {
 	AsOf time.Time
 	// Coin contains the common symbol of the coin.
 	Coin string
+	// Owned contains the amount of Coin owned.
+	Owned float64
 	// Fiat contains the common symbol of the fiat.
 	Fiat string
 	// Rate is the exchange rate "1 Coin = Rate Fiat".
@@ -27,15 +29,24 @@ type ExchangeRate struct {
 
 // String returns the ExchangeRate in a printable string representation.
 func (r *ExchangeRate) String() string {
+	if r.Owned > 0 {
+		return fmt.Sprintf("%f %s = %f %s (on %s as of %s)", r.Owned, r.Coin, (r.Owned * r.Rate), r.Fiat, r.ProviderWithMarket, r.AsOf.Format(time.RFC3339))
+	}
 	return fmt.Sprintf("1 %s = %f %s (on %s as of %s)", r.Coin, r.Rate, r.Fiat, r.ProviderWithMarket, r.AsOf.Format(time.RFC3339))
 }
 
 // StringCompact is a compact variant of String(), omitting time information.
 func (r *ExchangeRate) StringCompact() string {
+	if r.Owned > 0 {
+		return fmt.Sprintf("%f %s = %f %s (on %s)", r.Owned, r.Coin, (r.Owned * r.Rate), r.Fiat, r.ProviderWithMarket)
+	}
 	return fmt.Sprintf("1 %s = %f %s (on %s)", r.Coin, r.Rate, r.Fiat, r.ProviderWithMarket)
 }
 
 // StringVeryCompact is a very compact variant of String(), omitting time and provider information.
 func (r *ExchangeRate) StringVeryCompact() string {
+	if r.Owned > 0 {
+		return fmt.Sprintf("%f %s = %f %s", r.Owned, r.Coin, (r.Owned * r.Rate), r.Fiat)
+	}
 	return fmt.Sprintf("1 %s = %f %s", r.Coin, r.Rate, r.Fiat)
 }
