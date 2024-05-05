b4617667ea748b1d2149b318547442d75fed7e05
Robert WEILER <rbrt-weiler@mailbox.org>
2024-05-05 17:42:00 +0200

    Change version to 1.2.5-dev.

--- core/consts-vars.go
+++ core/consts-vars.go
@@ -10,7 +10,7 @@ const (
 	// ToolName contains the name of the application.
 	ToolName string = "Coinspy"
 	// ToolVersion contains the version number of the application.
-	ToolVersion string = "1.2.4"
+	ToolVersion string = "1.2.5-dev"
 	// ToolID contains a user-agent-style representation of ToolName/ToolVersion.
 	ToolID string = ToolName + "/" + ToolVersion
 	// ToolURL contains the URL where the application can be found.
